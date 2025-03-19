27eaf0478459a8788e0568d37070f4c1d1bcdfdc
saihusd <saihusd6@gmail.com>
2025-03-19 16:10:11 +0530

    Update constants.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,6 +2,6 @@ class Constants{
 
   String latitude = "1.332359766395726";
   String longitude = "103.89207915696818";
-  String apiKey = "1675c4ed02f69dfdd00d12b82e2482e9";
+  String apiKey = "";
 
 }
